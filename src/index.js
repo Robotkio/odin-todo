8fa28d91d30df40641388bb2873d924b2097d195
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-06-17 17:56:56 -0700

    delete everything, back to basics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import "./css/style.css";
-import { Item, Note, Project } from "./js/todo-data.js";
-import { makeItem } from "./js/todo-display.js";
+import { test } from "./js/todo-data";
 
-console.log("Hello world!");
-
-let item = new Item("A Grand Title", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dolores asperiores delectus debitis, cum magnam quod autem ullam perspiciatis ea vitae est eius molestias!", "DueDate", "high", "blue");
-item = makeItem(item);
-let container = document.querySelector(".item-container");
-console.log(item);
-container.appendChild(item);
+test();
--- src/js/todo-func.js
+++
@@ -1,6 +0,0 @@
-import "./todo-data.js";
-import { makeItem } from "./todo-display.js";
-
-function itemFunctions(item) {
-
-}
