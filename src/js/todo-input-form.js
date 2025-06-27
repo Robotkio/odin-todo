74ad4621a2e5ab9ec31f3cf3f54676036a1983cf
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-06-26 18:52:32 -0700

    place input form in popup window

--- src/js/todo-input-form.js
+++ src/js/todo-input-form.js
@@ -1,101 +1,123 @@
 
-export { addFormElement };
+export { todoForm };
 
-function addFormElement() {
-    const formDisplay = _buildScreenHider();
-    formDisplay.appendChild(_buildForm());
-    const body = document.body;
-    body.insertBefore(formDisplay, body.firstChild);
-}
+const todoForm = (function () {
+    function addFormElementToPage() {
+        const formDisplay = _buildScreenHider();
+        formDisplay.appendChild(_buildForm());
+        const body = document.body;
+        body.insertBefore(formDisplay, body.firstChild);
+    }
+    function addAddButtonToPage() {
+        const showForm = document.createElement("button");
+        showForm.classList.add("show-input-form-btn");
+        showForm.setAttribute("type", "button");
+        showForm.innerText = "Add";
+        document.body.insertBefore(showForm, document.body.firstChild);
+    }
+    function showForm() {
+        const form = document.querySelector(".screen-hider");
+        form.classList.remove("hide");
+        form.classList.add("show");
+    }
+    function hideForm() {
+        const form = document.querySelector(".screen-hider");
+        form.classList.remove("show");
+        form.classList.add("hide");
+    }
 
-function _buildScreenHider() {
-    const hider = document.createElement("div");
-    hider.classList.add("screen-hider");
-    return hider;
-}
+    function _buildScreenHider() {
+        const hider = document.createElement("div");
+        hider.classList.add("screen-hider");
+        hider.classList.add("hide");
+        return hider;
+    }
 
-function _buildForm() {
-    const form = document.createElement("form");
-    form.setAttribute("action", "#");
-    form.setAttribute("id", "item-form");
+    function _buildForm() {
+        const form = document.createElement("form");
+        form.setAttribute("action", "#");
+        form.setAttribute("id", "item-form");
 
-    /* title input */
-    const titleInput = document.createElement("div");
-    titleInput.classList.add("form-element");
+        /* title input */
+        const titleInput = document.createElement("div");
+        titleInput.classList.add("form-element");
 
-    let label = document.createElement("label");
-    label.setAttribute("for", "item-titl");
-    label.innerText = "Title";
+        let label = document.createElement("label");
+        label.setAttribute("for", "item-titl");
+        label.innerText = "Title";
 
-    let input = document.createElement("input");
-    input.setAttribute("type", "text");
-    input.setAttribute("id", "item-title");
-    input.setAttribute("name", "title");
+        let input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("id", "item-title");
+        input.setAttribute("name", "title");
 
-    titleInput.appendChild(label);
-    titleInput.appendChild(input);
+        titleInput.appendChild(label);
+        titleInput.appendChild(input);
 
-    /* date input */
-    const dateInput = document.createElement("div");
-    dateInput.classList.add("form-element");
+        /* date input */
+        const dateInput = document.createElement("div");
+        dateInput.classList.add("form-element");
 
-    label = document.createElement("label");
-    label.setAttribute("for", "item-date");
-    label.innerText = "Due Date";
+        label = document.createElement("label");
+        label.setAttribute("for", "item-date");
+        label.innerText = "Due Date";
 
-    input = document.createElement("input");
-    input.setAttribute("type", "date");
-    input.setAttribute("id", "item-date");
-    input.setAttribute("name", "date");
+        input = document.createElement("input");
+        input.setAttribute("type", "date");
+        input.setAttribute("id", "item-date");
+        input.setAttribute("name", "date");
 
-    dateInput.appendChild(label);
-    dateInput.appendChild(input);
+        dateInput.appendChild(label);
+        dateInput.appendChild(input);
 
-    /* description input */
-    const descInput = document.createElement("div");
-    descInput.classList.add("form-element");
+        /* description input */
+        const descInput = document.createElement("div");
+        descInput.classList.add("form-element");
 
-    label = document.createElement("label");
-    label.setAttribute("for", "item-description");
-    label.innerText = "Description";
+        label = document.createElement("label");
+        label.setAttribute("for", "item-description");
+        label.innerText = "Description";
 
-    input = document.createElement("textarea");
-    input.setAttribute("id", "item-description");
-    input.setAttribute("name", "desc");
+        input = document.createElement("textarea");
+        input.setAttribute("id", "item-description");
+        input.setAttribute("name", "desc");
 
-    descInput.appendChild(label);
-    descInput.appendChild(input);
+        descInput.appendChild(label);
+        descInput.appendChild(input);
 
-    /* buttons */
+        /* buttons */
 
-    const buttons = document.createElement("div");
-    buttons.classList.add("form-element");
+        const buttons = document.createElement("div");
+        buttons.classList.add("form-element");
 
-    const btnCancel = document.createElement("button");
-    btnCancel.setAttribute("type", "button");
-    btnCancel.classList.add("todo-cancel-btn");
-    btnCancel.innerText = "Cancel";
-    
-    const btnClear = document.createElement("button");
-    btnClear.setAttribute("type", "reset");
-    btnClear.classList.add("todo-clear-btn");
-    btnClear.innerText = "Clear";
+        const btnCancel = document.createElement("button");
+        btnCancel.setAttribute("type", "button");
+        btnCancel.classList.add("todo-cancel-btn");
+        btnCancel.innerText = "Cancel";
+        
+        const btnClear = document.createElement("button");
+        btnClear.setAttribute("type", "reset");
+        btnClear.classList.add("todo-clear-btn");
+        btnClear.innerText = "Clear";
 
-    const btnAdd = document.createElement("button");
-    btnAdd.setAttribute("type", "submit");
-    btnAdd.classList.add("todo-add-btn");
-    btnAdd.innerText = "Add";
+        const btnAdd = document.createElement("button");
+        btnAdd.setAttribute("type", "submit");
+        btnAdd.classList.add("todo-add-btn");
+        btnAdd.innerText = "Add";
 
-    buttons.appendChild(btnCancel);
-    buttons.appendChild(btnClear);
-    buttons.appendChild(btnAdd);
-    
-    /* bring it together */
+        buttons.appendChild(btnCancel);
+        buttons.appendChild(btnClear);
+        buttons.appendChild(btnAdd);
+        
+        /* bring it together */
 
-    form.appendChild(titleInput);
-    form.appendChild(dateInput);
-    form.appendChild(descInput);
-    form.appendChild(buttons);
+        form.appendChild(titleInput);
+        form.appendChild(dateInput);
+        form.appendChild(descInput);
+        form.appendChild(buttons);
 
-    return form;
-}
+        return form;
+    }
+
+    return { addFormElementToPage, addAddButtonToPage, showForm, hideForm }
+})();
--- src/js/todo.js
+++ src/js/todo.js
@@ -1,14 +1,17 @@
 import { format } from "../../node_modules/date-fns";
 import { TodoList } from "./todo-items";
-import { addFormElement } from "./todo-input-form";
+import { todoForm } from "./todo-input-form";
 
-addFormElement();
+
+todoForm.addFormElementToPage();
+todoForm.addAddButtonToPage();
 
 const formTitle = document.getElementById("item-title");
 const formDate = document.getElementById("item-date");
 const formDescription = document.getElementById("item-description");
 
 const displayArea = document.getElementById("main-display");
+const btnShowForm = document.querySelector(".show-input-form-btn");
 const btnAdd = document.querySelector(".todo-add-btn");
 const btnClear = document.querySelector(".todo-clear-btn");
 const btnCancel = document.querySelector(".todo-cancel-btn");
@@ -19,6 +22,7 @@ resetForm();
 btnAdd.addEventListener("click", (event) => {
     event.preventDefault();
     tl.addItemFromFormInputs();
+    todoForm.hideForm();
     resetForm();
 });
 
@@ -26,6 +30,17 @@ btnClear.addEventListener("click", (event) => {
     event.preventDefault();
     resetForm();
 });
+
+btnCancel.addEventListener("click", (event) => {
+    event.preventDefault();
+    resetForm();
+    todoForm.hideForm();
+});
+
+btnShowForm.addEventListener("click", (event) => {
+    event.preventDefault();
+    todoForm.showForm();
+})
 
 function resetForm() {
     formTitle.value = "";
